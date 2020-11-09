1be3c3fcd987c93fa2b7a773f8dd411d01b179bc
Jacob Geigle <jacob@geigle.me>
2020-11-09 17:43:45 -0500

    More status on tests during run

--- check/sensormv.sh
+++ check/sensormv.sh
@@ -1,20 +1,26 @@
 #/bin/sh
+echo "Starting emulator"
 ./example/tcip4_emulator --quiet --firmware-version 88 --sweep SENSORMV --pipe-name sensormv-tty &
 PID=$!
 while [ ! -e sensormv-tty ]
 do
 	sleep 0.1
+	echo "Waiting for pipe"
 done
+echo "starting check program"
 ./check/sensormv
 ret88=$?
 wait $PID
 echo V88: $ret88
+echo "Starting emulator"
 ./example/tcip4_emulator --quiet --firmware-version 96 --sweep SENSORMV --pipe-name sensormv-tty &
 PID=$!
 while [ ! -e sensormv-tty ]
 do
 	sleep 0.1
+	echo "Waiting for pipe"
 done
+echo "starting check program"
 ./check/sensormv
 ret96=$?
 wait $PID
